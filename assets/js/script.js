87a0163d6e57cb6cc4d81185c5f71f73785af388
Maria Fernanda Tella <mfgt160694@gmail.com>
2023-08-04 12:13:45 -0600

    actualizacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,47 @@
 console.log("pagina en cosntruccion con Patricio Estrella")
 
-function clickPatricio()[
+function clickPatricio(){
     alert("La mayonesa es un istrumento?");
-]
+}
+function changeColor(elementHTML, color){
+    console.log(elementHTML);
+    console.log(color);
+
+    elementHTML.style.color= color
+}
+
+function changeColorWithPromt(elementHTML){
+    const color= prompt("Dime el color en ingles", "yellow")
+    elementHTML.innerHTML= "<h3> Ahora soy <em>" + color + "</em> </h3>";
+    changeColor(elementHTML, color);
+}
+
+function changeText(element, color){
+    element.innerHTML = "<h3> Ahora soy <em>" + color + "</em> </h3>";
+
+}
+function changeName(){
+    const userName = prompt("Escribe tu nombre cholo", "Machete");
+    const refGretting = document.getElementById("gretting");
+    refGretting.innerHTML = `Hola ${userName}, la clika te respalda`;
+}
+
+
+
+
+
+
+
+
+
+function resetColor(){
+   const refRedColor = document.getElementById("red-color");
+   const refGreenColor = document.getElementById("green-color");
+   const refPurpleColor = document.getElementById("purple-color");
+   const refUserColor = document.getElementById("user-color");
+
+   changeColor(refRedColor, 'black');
+   changeColor(refGreenColor, 'black');
+   changeColor(refPurpleColor, 'black');
+   changeColor(refUserColor, 'black');
+}
