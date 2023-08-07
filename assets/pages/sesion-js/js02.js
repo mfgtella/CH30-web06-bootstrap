89d2f309ff75caba1be498eaf450c9e3ab54703b
Maria Fernanda Tella <mfgt160694@gmail.com>
2023-08-07 17:18:53 -0600

    rest parameters

--- assets/pages/sesion-js/js02.js
+++ assets/pages/sesion-js/js02.js
@@ -53,3 +53,73 @@ else {
 }    
 console.log(saludo("Fer"));
 
+/*
+--------- selft invoking functions------------
+se autoinvocan y se pueden definir con funciones anonimas. 
+
+sintaxis 
+( function(parametros) {} )(argumentos);
+*/
+(function (name){
+    console.log(`me llamo ${name} y me autoinvoco`);
+})("chatGPt");
+
+/*
+--------- arrow functions------------
+funciones similares a las funciones expresadas pero:
+    no requiere la palabra reservada function
+    si tiene una sola instruccion no requiere las llaves {}
+    si la instruccion es el mismo retorno, no requiere palabra return
+
+sintaxis 
+    const nomreVariable = (parametros) => instruccion;
+    const nomreVariable = (parametros) =>{
+        instruccion;
+        return expresion;
+    }
+*/
+
+/* const areaRectangulo = function (base,altura){
+    return base * altura;} */
+const areaRectangulo = (base,altura) => base * altura;
+    
+console.log(`El area es: ${areaRectangulo(3,9)}m2.`);
+
+
+/* 
+--------- parametros por defecto -------------
+inicializa un parametro de la funcion, si no se envia el argumento cuando se invoca
+ */
+
+/**
+ * calcula el area de un rectangulo 
+ * @param {number} base del triangulo
+ * @param {number} altura 
+ * @returns{number} base * altura/2
+ */
+const areaTriangulo = (base,altura) => base * altura/ 2.0;
+
+console.log(`el area de un triangulo es: ${areaTriangulo(2,4)}m2.`)
+console.log((1000).toString());
+console.log((1000).toString(2));
+console.log((1000).toString(16));
+
+/* 
+--------- rest parameters -------------
+permite representar una serie de valores indefinidos en los argumentos
+esta serie de valores se presentan como un array.
+Rest parameters dede de estar al final de la lista de parametros
+
+sintaxis:
+    ...nombreParametros
+
+ */
+const sumatoriaMultiplesNumeros = (a,b,...rest) => {
+    let suma = a + b;
+    for (let i = 0; i < rest.length; i++){
+        suma += rest[i]
+    }
+    return suma;
+}    
+console.log(`sumatoria multiples numeros: ${sumatoriaMultiplesNumeros(2,5)}`);
+console.log(`multiples numeros: ${sumatoriaMultiplesNumeros(2,5,7,8)}`);
