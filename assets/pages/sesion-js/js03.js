f9dee465b7ba072319920348286976d0f8c0f981
Maria Fernanda Tella <mfgt160694@gmail.com>
2023-08-09 11:06:24 -0600

    ci:declaracion bloque codigo

--- assets/pages/sesion-js/js03.js
+++ assets/pages/sesion-js/js03.js
@@ -1,6 +1,6 @@
 console.log("Sesion JS03 Control de flujo");
 console.log("No olvidaremos el reto de Ed");
-
+console.log("====================================");
 // ------------- Declaracion bloque de codigo--------
 /*
 sintaxis:
@@ -8,5 +8,31 @@ sintaxis:
 
         }
 las variables declaradas con let y const solo tendran alcance(scope)
-dentro del bloque o bloques anidados.        
-*/        
+dentro del bloque o bloques anidados.    
+
+si la variable en el bloque, tiene el mismo nombre
+que una variable fuera del bloque, se le da prioridad 
+a la variable dentro del bloque.
+*/
+let firstname = "Fer";
+const lastname = "Gomez"; 
+var age ="30"
+
+// let y const no se pueden redeclarar
+{
+    let firstname = "Mau";
+    const lastname = "Peniche"; 
+    var age ="26"
+    const colorFavorito ="azul";
+    var comidaFavorita = "pozole";
+
+    console.log(`${firstname} ${lastname}`);
+    console.log(age);
+    console.log(colorFavorito);
+    console.log(comidaFavorita);
+}       
+console.log(`${firstname} ${lastname}`);
+console.log(age);
+/* console.log(colorFavorito); */
+console.log(comidaFavorita);
+
